6c17166926885a822c65e2817a3d05912b762ac3
Max Alva <alva85@gmail.com>
2018-08-21 22:59:58 -0500

    Usando parciales con HBS

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 const express = require( 'express' );
+const hbs = require( 'hbs' );
 const app = express();
 
 app.use( express.static( __dirname + '/public' ) );
 
 // Express HBS engine
+hbs.registerPartials( __dirname + '/views/partials' );
 app.set( 'view engine', 'hbs' );
 
 app.get( '/', ( req, res ) => {
     res.render( 'home', {
         nombre: 'Max Alva',
+        year: new Date().getFullYear()
+    } );
+} );
+
+app.get( '/about', ( req, res ) => {
+    res.render( 'about', {
         year: new Date().getFullYear()
     } );
 } );
