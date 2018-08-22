805dfb141bb7283560f2a11c34e300aed27f794e
Max Alva <alva85@gmail.com>
2018-08-21 22:10:56 -0500

    servir cotenido estatico

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require( 'express' );
 const app = express();
 
-app.get( '/', ( req, res ) => {
-    res.send( 'Hola Mundo!' );
-} );
+app.use( express.static( __dirname + '/public' ) );
+
+/*app.get( '/', ( req, res ) => {
+ res.send( 'Hola Mundo!' );
+ } );*/
 
 app.listen( 3000, () => {
     console.log( 'Escuchando peticiones en el puerto 3000' );
